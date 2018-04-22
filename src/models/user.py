534349110c64ecfe3b0d89ab46bd7cb715017599
Robee <robert.filic@gmail.com>
2018-04-22 21:18:17 +0300

    Added search User by mail and ID

--- src/models/user.py
+++ src/models/user.py
@@ -1,17 +1,24 @@
-class User(object):
-    pass
+from src.common.database import Database
+
 
 class User(object):
-    def __init__(self):
-        pass
+    def __init__(self, email, password):
+        self. email = email
+        self.password = password
 
     # Filter users by email
-    def get_by_email(self):
-        pass
+    @classmethod
+    def get_by_email(cls, email):
+        data = Database.find_one("user",  {"email":email})
+        if data is not None:
+            return cls(**data)
 
     # Filter users by id/password
-    def get_by_id(self):
-        pass
+    @classmethod
+    def get_by_id(cls, _id):
+        data = Database.find_one("user", {"_id": _id})
+        if data is not None:
+            return cls(**data)
 
     #Check if user's email matches the password they sent us
     def login_valid(self):
@@ -22,4 +29,9 @@ class User(object):
 
     def login(self):
         pass
-    
+
+    def json(self):
+        pass
+
+    def save_to_mongo(self):
+        pass
