09d3f882f52359ccc4ba658c5407ec45aaa46aab
Robee <robert.filic@gmail.com>
2018-04-22 21:41:05 +0300

    Added method Register - user registration to the platform

--- src/models/user.py
+++ src/models/user.py
@@ -1,10 +1,13 @@
+import uuid
+
 from src.common.database import Database
 
 
 class User(object):
-    def __init__(self, email, password):
+    def __init__(self, email, password, _id=None):
         self. email = email
         self.password = password
+        self._id = uuid.uuid4().hex if _id is None else _id # assigning an ID to the user
 
     # Filter users by email
     @classmethod
@@ -19,13 +22,30 @@ class User(object):
         data = Database.find_one("user", {"_id": _id})
         if data is not None:
             return cls(**data)
+        #return None  this part is default in Python
 
+    @staticmethod
     #Check if user's email matches the password they sent us
-    def login_valid(self):
-        pass
+    def login_valid(email, password):
+        user = User.get_by_email(email) # if it finds the user in the database it will return an element, otherwise it will be None
+        if user is not None:
+            # Check the password
+            return user.password == password # compares password from database and the one entered in the website
+        return False
 
-    def register(self):
-        pass
+    @classmethod
+    def register(cls, email, password):
+        user = cls.get_by_email(email)
+        #check if the user already exists in our database
+        if user is None:
+            #if user doesn't exist, we can create it
+            new_user = cls(email, password)
+            new_user.save_to_mongo()
+            return True
+        else:
+            #User exists :(
+            return False
+
 
     def login(self):
         pass
